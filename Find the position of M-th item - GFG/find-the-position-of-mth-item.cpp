9e0d31ebdc35c745bd44e20283d6d43ef48eaed1
Harshill Kumar Dhanjal <90548564+harshiilll@users.noreply.github.com>
2022-01-15 22:49:39 +0530

    Added solution - LeetHub

---
+++ Find the position of M-th item - GFG/find-the-position-of-mth-item.cpp
@@ -0,0 +1,38 @@
+// { Driver Code Starts
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution {
+  public:
+    int findPosition(int N , int M , int K) {
+        // code here
+        if(N==1)
+            return 1;
+            
+        if((M+K-1)%N==0) // cycle complete
+            return N;
+        else
+            return (M+K-1)%N;
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,M,K;
+        
+        cin>>N>>M>>K;
+
+        Solution ob;
+        cout << ob.findPosition(N,M,K) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
